062c3a2bb1e083fb46a4de1b139a97762da7b31e
Tomasz Z <aquamin9@googlemail.com>
2021-05-11 18:21:11 +0200

    Update raw_data_downloader_TUM.sh
    updating dataloader

--- sample_data/dataset_TUM/raw_data_downloader_TUM.sh
+++ sample_data/dataset_TUM/raw_data_downloader_TUM.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+#freiburg1
 files=(rgbd_dataset_freiburg1_rpy.tgz
+rgbd_dataset_freiburg1_xyz.tgz
 )
+
 
 for i in ${files[@]}; do
                 fullname=$i
 	echo "Downloading: "$fullname
         wget 'https://vision.in.tum.de/rgbd/dataset/freiburg1/'$fullname
+        tar -xvzf $fullname
+        rm $fullname
+done
+
+#freiburg2
+files=(freiburg2_pioneer_slam.tgz
+freiburg2_pioneer_slam2.tgz
+)
+
+for i in ${files[@]}; do
+                fullname=$i
+	echo "Downloading: "$fullname
+        wget 'https://vision.in.tum.de/rgbd/dataset/freiburg2/'$fullname
         tar -xvzf $fullname
         rm $fullname
 done
