979f548a929a5f5b92873dc40066ef7f4223239d
Davide Plozza <dplozza@student.ethz.ch>
2021-05-27 15:06:08 +0200

    fixed autograd error when training without pose loss

--- end2endslam/losses/unified_loss.py
+++ end2endslam/losses/unified_loss.py
@@ -9,9 +9,9 @@ from end2endslam.losses.pose_loss import pose_loss_wrapper
 def pred_loss_unified(args, input_dict, slam, pointclouds, live_frame):
     loss = {"com": 0}
 
-    #if args.loss_photo_factor > 0 or args.loss_geom_factor > 0:
     loss["photo"], loss["geom"] = photo_and_geometry_loss_wrapper(args, input_dict)
-    loss["com"] += loss["photo"] * args.loss_photo_factor + loss["geom"] * args.loss_geom_factor
+    if args.loss_photo_factor > 0 or args.loss_geom_factor > 0:
+        loss["com"] += loss["photo"] * args.loss_photo_factor + loss["geom"] * args.loss_geom_factor
     if args.loss_smooth_factor > 0:
         loss["smooth"] = smooth_loss_wrapper(args, input_dict)
         loss["com"] += loss["smooth"] * args.loss_smooth_factor
@@ -21,10 +21,11 @@ def pred_loss_unified(args, input_dict, slam, pointclouds, live_frame):
     if args.loss_gt_factor > 0:
         loss["gt"] = gt_loss_wrapper(args, input_dict)
         loss["com"] += loss["gt"] * args.loss_gt_factor
-    #if args.loss_pose_trans_factor > 0 or args.loss_pose_rot_factor > 0:
+
     loss['pose_rot'],loss['pose_trans'] = pose_loss_wrapper(args, input_dict)
-    loss["com"] += loss["pose_rot"] * args.loss_pose_rot_factor
-    loss["com"] += loss["pose_trans"] * args.loss_pose_trans_factor
+    if args.loss_pose_trans_factor > 0 or args.loss_pose_rot_factor > 0:
+        loss["com"] += loss["pose_rot"] * args.loss_pose_rot_factor
+        loss["com"] += loss["pose_trans"] * args.loss_pose_trans_factor
 
     if args.loss_photo_factor + args.loss_geom_factor + args.loss_smooth_factor + args.loss_cons_factor + args.loss_gt_factor + args.loss_pose_rot_factor + args.loss_pose_trans_factor <= 0:
         raise ValueError("The loss multiplication factors are less than zero | Not training")
