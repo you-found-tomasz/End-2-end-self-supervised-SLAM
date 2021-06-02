f1f06cdaca907a8198de1e56c2dc7c8632e888ef
Davide Plozza <dplozza@student.ethz.ch>
2021-06-02 21:44:07 +0200

    train

--- end2endslam/auto_train_slam.sh
+++ end2endslam/auto_train_slam.sh
@@ -24,12 +24,37 @@ epochs=301
 
 
 sequence="rgbd_dataset_freiburg2_pioneer_slam"
+dilation=9
+stride=0
+lr="1e-06"
+batch=2
+max_batches=1
+odometry="gradicp"
+train_odometry="gt"
+bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 10 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_matthias_gt --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 571 --num_epochs ${epochs} --projection_mode previous --seq_stride ${stride} --learning_rate ${lr} --freeze ${freeze}
+
+
+sequence="rgbd_dataset_freiburg2_pioneer_slam"
+dilation=9
+stride=10
+lr="1e-06"
+batch=20
+max_batches=1
+odometry="gradicp"
+train_odometry="gt"
+bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_matthias_gt_seq2 --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 571 --num_epochs ${epochs} --projection_mode previous --seq_stride ${stride} --learning_rate ${lr} --freeze ${freeze}
+
+
+
+sequence="rgbd_dataset_freiburg2_pioneer_slam"
 dilation=19
 stride=20
 lr="5e-06"
 batch=2
 max_batches=10
-bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_batch10_slam_test_${dilation} --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 771 --num_epochs ${epochs} --projection_mode previous --seq_stride 101 --learning_rate ${lr} --freeze ${freeze}
+odometry="gradicp"
+train_odometry="slam"
+bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_batch10_slam_test_${dilation} --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 771 --num_epochs ${epochs} --projection_mode previous --seq_stride ${stride} --learning_rate ${lr} --freeze ${freeze}
 
 
 sequence="rgbd_dataset_freiburg2_pioneer_slam"
@@ -38,7 +63,7 @@ stride=20
 lr="5e-06"
 batch=4
 max_batches=5
-bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_batch10_slam_test_${dilation}_2 --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 771 --num_epochs ${epochs} --projection_mode previous --seq_stride 101 --learning_rate ${lr} --freeze ${freeze}
+bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_batch10_slam_test_${dilation}_2 --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 771 --num_epochs ${epochs} --projection_mode previous --seq_stride ${stride} --learning_rate ${lr} --freeze ${freeze}
 
 sequence="rgbd_dataset_freiburg2_pioneer_slam"
 dilation=29
@@ -46,4 +71,4 @@ stride=30
 lr="1e-05"
 batch=4
 max_batches=5
-bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_batch10_slam_test_${dilation}_3 --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 771 --num_epochs ${epochs} --projection_mode previous --seq_stride 101 --learning_rate ${lr} --freeze ${freeze}
+bsub -R "rusage[mem=8096, ngpus_excl_p=1]" python3 pointfusion_scsfm.py --odometry ${odometry} --train_odometry ${train_odometry} --dataset tum --dataset_path $path --sequences ${sequence} --seq_length 2 --batch_size ${batch} --debug_path ${debug_path} --model_name ${sequence}_batch10_slam_test_${dilation}_3 --loss_photo_factor 1 --loss_geom_factor 0.5 --loss_smooth_factor 0.1 --loss_cons_factor 0 --loss_gt_factor 0 --log_freq 50 --max_scale ${scale} --seq_dilation ${dilation} --max_num_batches ${max_batches} --seq_start 396 --seq_end 771 --num_epochs ${epochs} --projection_mode previous --seq_stride ${stride} --learning_rate ${lr} --freeze ${freeze}
